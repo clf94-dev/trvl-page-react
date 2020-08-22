93ec6a5eb0ed61eeb8c839bde8f552a8ede14f8f
clf94-dev <clf94@hotmail.es>
2020-08-22 11:39:07 +0200

    footer finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,11 @@ import Navbar from './Navbar';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './styles/App.css'
 import Home from '../components/pages/Home';
+import Products from './pages/Products';
+import Services from './pages/Services';
+import SignUp from './pages/SignUp';
+
+
 
 function App() {
     return (
@@ -12,6 +17,9 @@ function App() {
                 <Navbar/>
                 <Switch>
                     <Route path='/' exact component={Home}/>
+                    <Route path='/products' exact component={Products}/>
+                    <Route path='/services' exact component={Services}/>
+                    <Route path='/sign-up' exact component={SignUp}/>
                 </Switch>
 
             </Router>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,8 @@ import './styles/Footer.css'
 import {Button} from './Button'
 import {Link} from 'react-router-dom'
 
-function Footer() {
-    return (<>
-        <div className='footer-container'>
+export default function Footer() {
+    return ( <> <div className='footer-container'>
         <section className="footer-subscription">
             <p className="footer-subscription-heading">
                 Join the Adventure newsletter to receive our best vacation deals
@@ -59,62 +58,54 @@ function Footer() {
                     <Link to='/'>Twitter</Link>
                 </div>
             </div>
-            <section className="social-media">
-            <div className='social-media-wrap'>
-          <div className='footer-logo'>
-            <Link to='/' className='social-logo'>
-              TRVL
-              <i className='fab fa-typo3' />
-            </Link>
-          </div>
-          <small className='website-rights'>TRVL © 2020</small>
-          <div className='social-icons'>
-            <Link
-              class='social-icon-link facebook'
-              to='/'
-              target='_blank'
-              aria-label='Facebook'
-            >
-              <i className='fab fa-facebook-f' />
-            </Link>
-            <Link
-              class='social-icon-link instagram'
-              to='/'
-              target='_blank'
-              aria-label='Instagram'
-            >
-              <i className='fab fa-instagram' />
-            </Link>
-            <Link
-              class='social-icon-link youtube'
-              to='/'
-              target='_blank'
-              aria-label='Youtube'
-            >
-              <i className='fab fa-youtube' />
-            </Link>
-            <Link
-              class='social-icon-link twitter'
-              to='/'
-              target='_blank'
-              aria-label='Twitter'
-            >
-              <i className='fab fa-twitter' />
-            </Link>
-            <Link
-              class='social-icon-link twitter'
-              to='/'
-              target='_blank'
-              aria-label='LinkedIn'
-            >
-              <i className='fab fa-linkedin' />
-            </Link>
-          </div>
         </div>
-            </section>
+        <section className="social-media">
+            <div className='social-media-wrap'>
+                <div className='footer-logo'>
+                    <Link to='/' className='social-logo'>
+                        TRVL
+                        <i className='fab fa-typo3'/>
+                    </Link>
+                </div>
+                <small className='website-rights'>TRVL © 2020</small>
+                <div className='social-icons'>
+                    <Link
+                        class='social-icon-link facebook'
+                        to='/'
+                        target='_blank'
+                        aria-label='Facebook'>
+                        <i className='fab fa-facebook-f'/>
+                    </Link>
+                    <Link
+                        class='social-icon-link instagram'
+                        to='/'
+                        target='_blank'
+                        aria-label='Instagram'>
+                        <i className='fab fa-instagram'/>
+                    </Link>
+                    <Link
+                        class='social-icon-link youtube'
+                        to='/'
+                        target='_blank'
+                        aria-label='Youtube'>
+                        <i className='fab fa-youtube'/>
+                    </Link>
+                    <Link
+                        class='social-icon-link twitter'
+                        to='/'
+                        target='_blank'
+                        aria-label='Twitter'>
+                        <i className='fab fa-twitter'/>
+                    </Link>
+                    <Link
+                        class='social-icon-link twitter'
+                        to='/'
+                        target='_blank'
+                        aria-label='LinkedIn'>
+                        <i className='fab fa-linkedin'/>
+                    </Link>
+                </div>
             </div>
-    </div> </>
-    );
-}
-
-export default Footer;
+        </section>
+    </div> < />)
+        }
